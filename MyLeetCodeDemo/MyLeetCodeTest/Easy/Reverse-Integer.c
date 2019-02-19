4fbd8e275d4429662bf7411ac1cbdc71f26dba88
houboye <houboye@outlook.com>
2019-02-19 14:40:41 +0800

    reverse-integer方法完善

--- MyLeetCodeDemo/MyLeetCodeTest/Easy/Reverse-Integer.c
+++ MyLeetCodeDemo/MyLeetCodeTest/Easy/Reverse-Integer.c
@@ -2,13 +2,22 @@
 
 
 int reverse(int x) {
-    
-    
-    return 1;
+    if (x == 0) {
+        return 0;
+    }
+    int res = 0;
+    int num = x;
+    while (num != 0) {
+        int tmp = num % 10;
+        
+        res = res * 10 + tmp;
+        num = num/10;
+    }
+    return res;
 }
 
 void reverseIntegerExample(void) {
-    int target = 123;
+    int target = -12305;
     printf("target=%d, result=%d\n", target, reverse(target));
     
 }
